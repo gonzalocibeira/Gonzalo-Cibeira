43f60973ad7707e84190f34f6982d0c663d69aff
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-14 23:22:18 +0200

    merged new change lang function

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ const esBtn = document.getElementById("esBtn");
 const contents = [esCont, enCont]
 const btns = [esBtn, enBtn]
 
+/* Function to toggle display state */
 function toggleDisp(cont){
     window.getComputedStyle(cont[0]).display === "none" ? cont.forEach(el => el.style.display = "block" ) : cont.forEach(el => el.style.display = "none");
 }
 
+/* Function to toggle lang button state */
 function toggleBtn(btn){
     window.getComputedStyle(btn).fontWeight === "700" ? btn.style.fontWeight = "100" : btn.style.fontWeight = "700";
     console.log(window.getComputedStyle(btn).fontWeight);
