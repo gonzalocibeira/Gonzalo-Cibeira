0fd905b1586cd24919c4cfbf9e41d413ad7389d1
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-14 23:12:03 +0200

    func changes

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ const enCont = document.querySelectorAll('.enText');
 const enBtn = document.getElementById("enBtn");
 const esBtn = document.getElementById("esBtn");
 
+const contents = [esCont, enCont]
+const btns = [esBtn, enBtn]
+
+function toggleDisp(cont){
+    window.getComputedStyle(cont[0]).display === "none" ? cont.forEach(el => el.style.display = "block" ) : cont.forEach(el => el.style.display = "none");
+}
+
+function toggleBtn(btn){
+    window.getComputedStyle(btn).fontWeight === "bolder" ? btn.style.fontWeight = "lighter" : btn.style.fontWeight = "bolder";
+    console.log(window.getComputedStyle(btn).fontWeight);
+}
+
+function changeLang(){
+    contents.forEach(cont => toggleDisp(cont));
+    btns.forEach(btn => toggleBtn(btn));
+}
