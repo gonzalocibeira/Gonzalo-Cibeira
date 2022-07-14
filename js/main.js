74f6642fec1543f66232c2a28e793252a1a05acc
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-14 17:40:57 +0200

    lang changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const enCont = document.querySelectorAll('.enText');
 const enBtn = document.getElementById("enBtn");
 const esBtn = document.getElementById("esBtn");
 
-
 function changeLang(){
     if (esCont[0].style.display === "none") {
         esCont.forEach(el => el.style.display = "block");
@@ -19,4 +18,3 @@ function changeLang(){
     }
 
 }
-
