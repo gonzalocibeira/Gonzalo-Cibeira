0851392b3ef31c624e9240e18f47955f6960587f
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-14 22:52:26 +0200

    new lang func

--- js/main.js
+++ js/main.js
@@ -3,20 +3,3 @@ const enCont = document.querySelectorAll('.enText');
 const enBtn = document.getElementById("enBtn");
 const esBtn = document.getElementById("esBtn");
 
-function setLangEs(){
-    esCont.forEach(el => el.style.display = "block");
-    enCont.forEach(el => el.style.display = "none");
-    esBtn.style.fontWeight = "bolder";
-    enBtn.style.fontWeight = "lighter"
-};
-
-function setLangEn(){
-    esCont.forEach(el => el.style.display = "none");
-    enCont.forEach(el => el.style.display = "block");
-    esBtn.style.fontWeight = "lighter";
-    enBtn.style.fontWeight = "bolder";
-};
-
-function changeLang(){
-    esCont[0].style.display === "none" ? setLangEs() : setLangEn();
-}
