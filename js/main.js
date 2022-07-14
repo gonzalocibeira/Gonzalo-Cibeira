ed75023ca076fa7eb5e17565a97eaa7bf217b219
turcocibeira <cibeiragonzalo@gmail.com>
2022-07-14 21:15:41 +0200

    lang fixes

--- js/main.js
+++ js/main.js
@@ -3,18 +3,20 @@ const enCont = document.querySelectorAll('.enText');
 const enBtn = document.getElementById("enBtn");
 const esBtn = document.getElementById("esBtn");
 
-function changeLang(){
-    if (esCont[0].style.display === "none") {
-        esCont.forEach(el => el.style.display = "block");
-        enCont.forEach(el => el.style.display = "none");
-        esBtn.style.fontWeight = "bolder";
-        enBtn.style.fontWeight = "lighter";
-    }
-    else {
-        esCont.forEach(el => el.style.display = "none");
-        enCont.forEach(el => el.style.display = "block");
-        esBtn.style.fontWeight = "lighter";
-        enBtn.style.fontWeight = "bolder";
-    }
+function setLangEs(){
+    esCont.forEach(el => el.style.display = "block");
+    enCont.forEach(el => el.style.display = "none");
+    esBtn.style.fontWeight = "bolder";
+    enBtn.style.fontWeight = "lighter"
+};
 
+function setLangEn(){
+    esCont.forEach(el => el.style.display = "none");
+    enCont.forEach(el => el.style.display = "block");
+    esBtn.style.fontWeight = "lighter";
+    enBtn.style.fontWeight = "bolder";
+};
+
+function changeLang(){
+    esCont[0].style.display === "none" ? setLangEs() : setLangEn();
 }
